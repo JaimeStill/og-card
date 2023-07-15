267c1b956dabcb18506cad2d74d2c469ebfc23f2
Jaime Still <jpstill85@gmail.com>
2023-07-15 13:40:23 -0400

    Create card-padding-fix.jsx

---
+++ designs/03-starfield/card-padding-fix.jsx
@@ -0,0 +1,71 @@
+<div
+  style={{
+    height: '100%',
+    width: '100%',
+    display: 'flex',
+    backgroundColor: '#ffffff',
+    fontSize: 32,
+    fontWeight: 600,
+    margin: 40,
+    border: '6px solid #333333'
+  }}
+>
+  <div
+    style={{
+      height: '100%',
+      width: 24,
+      backgroundColor: '#cb213b'
+    }}
+  >
+  </div>
+  <div
+    style={{
+      height: '100%',
+      width: 24,
+      backgroundColor: '#e16235'
+    }}
+  >
+  </div>
+  <div
+    style={{
+      height: '100%',
+      width: 24,
+      backgroundColor: '#deaa47'
+    }}
+  >
+  </div>  
+  <div
+    style={{
+      height: '100%',
+      width: 24,
+      backgroundColor: '#305282'
+    }}
+  >
+  </div>
+  <div
+    style={{
+      display: 'flex',
+      flex: '1 1 auto',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center'
+    }}
+  >
+    <img
+      src="https://avatars.githubusercontent.com/u/14102723?v=4"
+      height="260"
+      style={{
+        borderRadius: '50%',
+        border: '6px solid #333333'
+      }}
+    />
+    <div
+      style={{
+        marginTop: 40,
+        color: '#333333',
+      }}
+    >
+      https://jaimestill.github.io/og-card
+    </div>
+  </div>
+</div>
